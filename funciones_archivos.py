8a86f65ca5149b82657e2e92933f92f753f9c50b
julgallo1 <julgallo@uade.edu.ar>
2025-10-06 21:14:19 -0300

    archivos.py - version 1

---
+++ funciones_archivos.py
@@ -0,0 +1,26 @@
+def cargar_datos(nombre_archivo):
+    lista_productos = []
+    try:
+        with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+            for linea in archivo:
+                datos = linea.strip().split(",")
+
+                if len(datos) == 4:
+                    producto = {
+                        "modelo": datos[0],
+                        "talle": int(datos[1]),
+                        "cantidad": int(datos[2]),
+                        "precio": float(datos[3])
+                    }
+                    lista_productos.append(producto)
+    except FileNotFoundError:
+        print(f"No se encontró el archivo {nombre_archivo}. Se creará uno nuevo al guardar los datos.")
+    return lista_productos
+
+
+# Guarda la lista de productos en el archivo
+def guardar_datos(nombre_archivo, lista_productos):
+    with open(nombre_archivo, "w", encoding="utf-8") as archivo:
+        for producto in lista_productos:
+            linea = f"{producto['modelo']},{producto['talle']},{producto['cantidad']},{producto['precio']}\n"
+            archivo.write(linea)
